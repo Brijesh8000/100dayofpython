eae8dc72f4b1cace6d442505fec6e5f877e3c8a5
Brijesh8000 <brijeshshaw8000@gmail.com>
2024-04-02 12:00:20 +0530

    file modify

--- day_5.py
+++ day_5.py
@@ -1,3 +1,16 @@
-fruits=["Apple","Peeach","Pear"]
-for fruit in fruits:
-    print(fruit)
+'''Day 5 '''
+
+student_heights = [180, 124, 165, 173, 189, 169, 146]
+total_height=0
+no_of_student=0
+for i in student_heights:
+    total_height+=i
+    no_of_student+=1
+print(total_height)
+print(no_of_student)
+
+average_height=total_height/no_of_student
+print(round(average_height))
+# fruits=["Apple","Peeach","Pear"]
+# for fruit in fruits:
+#     print(fruit)
