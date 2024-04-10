39e2883e1d6bc84fadf1451c123b0f9df8d639c2
Brijesh8000 <brijeshshaw8000@gmail.com>
2024-04-10 20:56:57 +0530

    modify file

--- day_13.py
+++ day_13.py
@@ -23,24 +23,30 @@
 ''' Debuging Complited '''
 
 # # Fix the Errors
-# age = input("How old are you?")
+# age = int(input("How old are you?"))
 # if age > 18:
-# print("You can drive at age {age}.")
+#    print("You can drive at age {age}.")
+
+''' Debuging Complited '''
 
 # #Print is Your Friend
 # pages = 0
 # word_per_page = 0
 # pages = int(input("Number of pages: "))
-# word_per_page == int(input("Number of words per page: "))
+# word_per_page = int(input("Number of words per page: "))
 # total_words = pages * word_per_page
 # print(total_words)
+''' Debuging Complited '''
 
-# #Use a Debugger
+
+# # Use a Debugger
 # def mutate(a_list):
 #   b_list = []
 #   for item in a_list:
 #     new_item = item * 2
-#   b_list.append(new_item)
+#     b_list.append(new_item)
 #   print(b_list)
 
 # mutate([1,2,3,5,8,13])
+
+''' Debuging Complited '''
