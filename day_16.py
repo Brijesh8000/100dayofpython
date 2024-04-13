f3c9fc345173f33c25c27b76e7b335eb14516dfe
Brijesh8000 <brijeshshaw8000@gmail.com>
2024-04-13 22:07:43 +0530

    Modify File

---
+++ day_16.py
@@ -0,0 +1,10 @@
+from turtle import Turtle ,Screen
+import day_16_module
+
+print(day_16_module.anathor_variable)
+
+timmy=Turtle()
+
+my_screen=Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
---
+++ day_16_module.py
@@ -0,0 +1,1 @@
+anathor_variable=12
