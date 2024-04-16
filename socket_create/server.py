ff16e67382718b3d54b57f82c1e32efb1dac49a6
Brijesh8000 <brijeshshaw8000@gmail.com>
2024-04-16 15:41:45 +0530

    socket created

---
+++ socket_create/server.py
@@ -0,0 +1,12 @@
+import socket
+s = socket.socket()
+print(("socket created"))
+s.bind(('localhost',9999))
+s.listen(3)
+print("waiting for connection ")
+while True:
+    c, addr = s.accept()
+    print("connected with : ", addr)
+    c.sent("you are connected ")
+    c.close()
+
