6a4711342d6fe07bc59c978a65233cf8cdeb709d
Brijesh8000 <brijeshshaw8000@gmail.com>
2024-05-07 14:28:48 +0530

    febonacci

--- fabonacci_series.py
+++ fabonacci_series.py
@@ -1,13 +1,48 @@
-def fibo(i):
+"""Febonacci Series using recursion """
+
+def fun(i):
     if i<=1:
         return i
     else:
-        return fibo(i-1)+fibo(i-2)
-n=int(input("Enter A number here : "))
+        return fun(i-1)+fun(i-2)  
+n=5
 if n<=0:
     print("Enter a positive number : ")
 else:
-    for i in range(n):
-        print(fibo(i))
-    
+    print("Febonacci sequence ")
+    for i in range(1,n+1):
+        print(fun(i))
+
+
+"""Febonacci Series using for loop """
+
+def febo(n ) :
+      if n<=0:
+            print("Enter a positive no : ")
+            return
+      else:
+            a=0
+            b=1
+            if n==1:
+                  print(a)
+            else:
+                  print(a)
+                  print(b)
+                  for i in range(2,n):
+                        c=a+b
+                        a=b
+                        b=c
+                        print(c)
+
+
+
+
+n=int(input("Enter the no : "))
+febo(n)
+      
+
+
+
+
+ 
     
